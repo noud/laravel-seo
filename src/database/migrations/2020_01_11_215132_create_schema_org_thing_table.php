d06929614f04896774a3299bb9d0ef6624a467f7
Noud de Brouwer <noud4@home.nl>
2020-01-11 22:58:09 +0100

    Thing

--- src/database/migrations/2020_01_11_215132_create_schema_org_thing_table.php
+++ src/database/migrations/2020_01_11_215132_create_schema_org_thing_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateThingTable extends Migration
+class CreateSchemaOrgThingTable extends Migration
 {
     /**
      * Run the migrations.
